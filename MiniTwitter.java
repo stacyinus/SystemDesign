c7cdc3bd97b658f285b14b6c1f8e7213ed528552
stacyinus <shenyue.2010@gmail.com>
2016-03-29 18:05:52 -0400

    mini twitter

---
+++ MiniTwitter.java
@@ -0,0 +1,64 @@
+/*
+Implement a simple twitter. Support the following method:
+
+postTweet(user_id, tweet_text). Post a tweet.
+getTimeline(user_id). Get the given user's most recently 10 tweets posted by himself, order by timestamp from most recent to least recent.
+getNewsFeed(user_id). Get the given user's most recently 10 tweets in his news feed (posted by his friends and himself). Order by timestamp from most recent to least recent.
+follow(from_user_id, to_user_id). from_user_id followed to_user_id.
+unfollow(from_user_id, to_user_id). from_user_id unfollowed to to_user_id.
+
+*/
+
+/**
+ * Definition of Tweet:
+ * public class Tweet {
+ *     public int id;
+ *     public int user_id;
+ *     public String text;
+ *     public static Tweet create(int user_id, String tweet_text) {
+ *         // This will create a new tweet object,
+ *         // and auto fill id
+ *     }
+ * }
+ */
+public class MiniTwitter {
+
+    public MiniTwitter() {
+        // initialize your data structure here.
+    }
+
+    // @param user_id an integer
+    // @param tweet a string
+    // return a tweet
+    public Tweet postTweet(int user_id, String tweet_text) {
+        //  Write your code here
+    }
+
+    // @param user_id an integer
+    // return a list of 10 new feeds recently
+    // and sort by timeline
+    public List<Tweet> getNewsFeed(int user_id) {
+        // Write your code here
+    }
+        
+    // @param user_id an integer
+    // return a list of 10 new posts recently
+    // and sort by timeline
+    public List<Tweet>  getTimeline(int user_id) {
+        // Write your code here
+    }
+
+    // @param from_user_id an integer
+    // @param to_user_id an integer
+    // from user_id follows to_user_id
+    public void follow(int from_user_id, int to_user_id) {
+        // Write your code here
+    }
+
+    // @param from_user_id an integer
+    // @param to_user_id an integer
+    // from user_id unfollows to_user_id
+    public void unfollow(int from_user_id, int to_user_id) {
+        // Write your code here
+    }
+}
